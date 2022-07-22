9cabbab63a06b00c0d6497a82fbcde99df0182fa
Duy Pham <ericpham19@outlook.com>
2022-07-22 14:58:25 +1000

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+function writeCards(name , fun ) {
+    let birthdayReply = [];
+    for ( let i = 0; i < name.length; i++) {
+        //birthdayReply =(`Thank you, ${name[i]}, for the wonderful ${fun} gift!`);
+
+        let a = (`Thank you, ${name[i]}, for the wonderful ${fun} gift!`)
+        birthdayReply.push(a);
+        
+    }
+  
+    return birthdayReply
+}
+
+
+function countDown() {
+    let countDown= 10;
+    for (countDown = 10 ; countDown >= 0 ; countDown--){
+        console.log(countDown);
+    }
+}
+
